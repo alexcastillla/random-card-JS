e9e616923d2cadb16a279111553fcb3efd6f3142
alexcastillla <alexander.zcas@gmail.com>
2020-10-14 16:18:56 +0000

    funcition added to the js file

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,39 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+// window.onload = function() {
+//   console.log("Hello Rigo from the console!");
+// };
+
+// El Ejercicio empieza aquí
+
+var submitbutton = document.querySelector("#form-submit");
+console.log(submitbutton);
+submitbutton.onclick = function() {
+  pepe();
 };
+
+function pepe() {
+  var nameImput = document.querySelector("#first-name").value;
+  var lastnameImput = document.querySelector("#last-name").value;
+  var cardImput = document.querySelector("#cardNumber").value;
+  var holaquetal = document.querySelector("#inputCVV").value;
+  var amountImput = document.querySelector("#imputAmount").value;
+  var cityImput = document.querySelector("#city-name").value;
+  var postalImput = document.querySelector("#postal-code").value;
+
+  if (
+    nameImput == "" ||
+    lastnameImput == "" ||
+    cardImput == "" ||
+    holaquetal == "" ||
+    amountImput == "" ||
+    cityImput == "" ||
+    postalImput == ""
+  ) {
+    alert("Rellenar datos vacios!");
+  } else {
+    alert("Ya esta completo!");
+  }
+  console.log(36);
+}
