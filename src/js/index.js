967a113b911c45f4daa042373a6b6635fd563800
alexcastillla <alexander.zcas@gmail.com>
2020-10-15 10:07:14 +0000

    Corregido estructura y nombre de variables

--- src/js/index.js
+++ src/js/index.js
@@ -5,34 +5,24 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-// El Ejercicio empieza aquí
+// JS empieza desdde aquí
 
-// var submitbutton = document.querySelector("#form-submit");
-// console.log(submitbutton);
-// submitbutton.onclick = function() {
-//   pepe();
-// };
 document.querySelector("#submit").onclick = function() {
-  pepe();
+  validation();
 };
 
-function pepe() {
-  var nameImput = document.querySelector("#first-name").value;
-  var lastnameImput = document.querySelector("#last-name").value;
-  var cardImput = document.querySelector("#cardNumber").value;
-  var holaquetal = document.querySelector("#inputCVV").value;
-  var amountImput = document.querySelector("#imputAmount").value;
-  var cityImput = document.querySelector("#city-name").value;
-  var postalImput = document.querySelector("#postal-code").value;
+function validation() {
+    var nameImput = document.querySelector("#first-name").value;
+    var lastnameImput = document.querySelector("#last-name").value;
+    var cardImput = document.querySelector("#cardNumber").value;
+    var inputCVV = document.querySelector("#inputCVV").value;
+    var amountImput = document.querySelector("#imputAmount").value;
+    var cityImput = document.querySelector("#city-name").value;
+    var postalImput = document.querySelector("#postal-code").value;
 
   if (
-    nameImput == "" ||
-    lastnameImput == "" ||
-    cardImput == "" ||
-    holaquetal == "" ||
-    amountImput == "" ||
-    cityImput == "" ||
-    postalImput == ""
+    nameImput == "" || lastnameImput == "" || cardImput == "" || inputCVV == "" ||
+    amountImput == "" || cityImput == "" || postalImput == ""
   ) {
     error.style.display = "inline";
     success.style.display = "none";
